ca3529412ccb97017451aec0f3d72a3d889c8f9b
shiniefg <shinitizui@gmail.com>
2025-10-17 11:49:41 +0000

    trazendo projeto

---
+++ px.js
@@ -0,0 +1,21 @@
+function nb() {
+    window.location.href = "px2.html"
+}
+function mapa() {
+    window.location.href = "px2.html"
+}
+function b1() {
+    window.location.href = "pxhistoriaa.html"
+}
+function hi() {
+    window.location.href = "px2.html"
+}
+function hi() {
+    window.location.href = "px2.html"
+}function b2() {
+    window.location.href = "pxpersonagem.html"
+}
+function b3() {
+    window.location.href = "pxmod.html"
+}
+
