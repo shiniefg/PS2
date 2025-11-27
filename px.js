91fba2a42562b666139f7e45079b05a0c4e58943
shiniefg <shinitizui@gmail.com>
2025-11-27 15:05:31 +0000

    a

--- px.js
+++ px.js
@@ -1,3 +1,18 @@
 function nb() {
     window.location.href = "px2.html";
 }
+function b1() {
+    window.location.href = "pxhistoria.html";
+}
+
+function b2() {
+    window.location.href = "pxpersonagens.html";
+}
+
+function b3() {
+    window.location.href = "pxmod.html";
+}
+
+function ha() {
+    window.location.href = "ha.html";
+}
